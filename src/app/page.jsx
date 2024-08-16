dc94ecf7d71d534d5732492a6d1f9da0bd1476da
MasterTraits <emmanuelfabella606@gmail.com>
2024-08-16 16:40:33 +0800

    removed nextui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
-import { Button } from "@nextui-org/react"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello guys
+    <main className="h-screen absolute w-screen bg-gradient-to-t from-white to-black top-0 left-0 z-[-5] flex justify-center items-center">
+      <div className="w-6/8">
+        <h1 className="text-center text-3xl font-bold mb-7 z-10">Welcome</h1>
+        <h2 className="text-center text-xl mb-5 z-10">Sign-In</h2>
+
+        <input className="mb-5" radius="lg" size="md" type="email" label="email" color="default" isRequired/>
+        <input className="mb-7" size="md" type="password" radius="lg" label="password" color="default" isRequired/>
+        <Link href="/forgotpass" className="underline cursor-pointer z-10">Forgot Password?</Link>
+        
+      </div>
     </main>
   );
 }
+
+
