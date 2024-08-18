6d2dfd591a8c755060e2e66a2e7e48f86e76a862
Shunke4444 <jihadtejam867@gmail.com>
2024-08-18 14:20:18 +0800

    Navbar finished

---
+++ src/components/navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BsBriefcase } from "react-icons/bs";
+import { FiUser } from "react-icons/fi";
+import { GoFlame } from 'react-icons/go';
+import { PiGraduationCap } from "react-icons/pi";
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+
+    <div className='fixed bottom-0 w-full bg-navbar text-white'>
+      <div className='flex justify-around py-4 px-2'>
+        <Link href="/dashboard" passHref>
+          <div className='flex flex-col items-center cursor-pointer'>
+            <PiGraduationCap className="text-2xl" /> 
+            <span className='text-sm font-extralight tracking-wider'>Learn</span>
+          </div>
+        </Link>
+        
+        <Link href="/shorts" passHref>
+          <div className='flex flex-col items-center cursor-pointer'>
+            <GoFlame className="text-2xl"/>
+            <span className='text-sm font-extralight tracking-wider'>Shorts</span>
+          </div>
+        </Link>
+
+        <Link href="/mentor" passHref>
+          <div className='flex flex-col items-center cursor-pointer'>
+            <BsBriefcase className="text-2xl" />
+            <span className='text-sm font-extralight tracking-wider'>Mentor</span>
+          </div>
+        </Link>
+
+        <Link href="/profile" passHref>
+          <div className='flex flex-col items-center cursor-pointer'>
+            <FiUser className="text-2xl" />
+            <span className='text-sm font-extralight tracking-wider'>Profile</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
