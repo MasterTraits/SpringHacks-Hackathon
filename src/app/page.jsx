94dc37bf7e93af51007c5d7e3a7c923c4d961a95
MasterTraits <emmanuelfabella606@gmail.com>
2024-08-18 16:54:55 +0800

    doing landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,67 +1,12 @@
-'use client'
 
-import Divider from "@/assets/Divider";
-import Link from "next/link";
-import { useState } from 'react'
-import { FiEye, FiEyeOff } from "react-icons/fi";
+import Divider from "@/assets/Divider"
 
-export default function Home() {
-  const [showpwd, setShowPwd] = useState({
-    seepwd: false,
-    text: "password"
-  });  
-
-  function togglePwd() {
-    setShowPwd(prevState => ({
-      seepwd: !prevState.seepwd,
-      text: prevState.seepwd ? "password" : "text"
-    }));
-  }
-
+export default function page() {
   return (
-    // The Main Container
-    <main className="h-screen flex justify-center bg-gradient-to-r from-blue-400 to-green-400">
-      {/* The Background Image */}
-      <img className="absolute h-screen w-full object-cover object-right z-[0]" src="https://images.pexels.com/photos/7130475/pexels-photo-7130475.jpeg"/>
-      <Divider output="absolute w-full overflow-hidden bottom-0 object-cover"/>
-
-      {/* The Content Itself */}
-      <div className="flex flex-col justify-between items-center w-11/12 p-5 py-7 *:z-20">
-        <h1 className="text-xl  mt-3">EduSphere</h1>
-        <div>
-          <h2 className="text-center text-2xl font-bold mb-5 ">Let's Sign You In!</h2>
-          <form>
-            <input className="mb-4 input input-bordered rounded-full bg-transparent w-full max-w-md" 
-              type="email" 
-              placeholder="Email" 
-              required
-            />
-            <br/>
-            <div className="relative">
-              <input className="mb-7 input input-bordered rounded-full bg-transparent w-full max-w-md" 
-                type={showpwd.text}  
-                placeholder="Password"
-                required
-              />
-              {showpwd.seepwd ? 
-                <FiEye className="absolute top-4 right-4" onClick={togglePwd}/> 
-              : <FiEyeOff className="absolute top-4 right-4" onClick={togglePwd}/>}
-            </div>
-          </form>
-          
-          <Link href="/forgotpass" className="underline text-sm">Forgot Password?</Link>
-          <br/><br/>
-          <div className="flex"> 
-            <input type="checkbox" className="checkbox border-2 h-5 w-5" id="remember"/>
-            <label className="text-sm" htmlFor="remember">&nbsp; Remember Me</label>
-          </div>
-          
-          <button className="btn w-full max-w-md mt-10 rounded-full">Sign In</button> 
-
-        </div>
-        {/* Empty mf */}
-        <div></div>
+    <main className='h-screen flex justify-center items-center bg-background'>
+      <div>
+        
       </div>
     </main>
-  );
+  )
 }
